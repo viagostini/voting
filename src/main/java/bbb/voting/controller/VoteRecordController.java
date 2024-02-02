d6bec619791ef862d4c519c9aca858fc2c917079
Vinicius Agostini <vinicius.agostini19@gmail.com>
2024-02-02 11:21:25 +0100

    refactor: add pagination to vote-records

--- src/main/java/bbb/voting/controller/VoteRecordController.java
+++ src/main/java/bbb/voting/controller/VoteRecordController.java
@@ -2,9 +2,15 @@ package bbb.voting.controller;
 
 import bbb.voting.entity.VoteRecord;
 import bbb.voting.repository.VoteRecordRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/vote-records")
@@ -16,7 +22,17 @@ public class VoteRecordController {
     }
 
     @GetMapping
-    public Iterable<VoteRecord> getVoteRecords() {
-        return voteRecordRepository.findAll();
+    public ResponseEntity<Map<String, Object>> getVoteRecords(@RequestParam(defaultValue = "0") int page,
+                                                              @RequestParam(defaultValue = "10") int size) {
+        Page<VoteRecord> pageVoteRecords = voteRecordRepository.findAll(PageRequest.of(page, size));
+
+        Map<String, Object> response = Map.of(
+            "voteRecords", pageVoteRecords.getContent(),
+            "currentPage", pageVoteRecords.getNumber(),
+            "totalItems", pageVoteRecords.getTotalElements(),
+            "totalPages", pageVoteRecords.getTotalPages()
+        );
+
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/bbb/voting/repository/VoteRecordRepository.java
+++ src/main/java/bbb/voting/repository/VoteRecordRepository.java
@@ -2,10 +2,13 @@ package bbb.voting.repository;
 
 import bbb.voting.entity.VoteRecord;
 import bbb.voting.entity.VoteRecordStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface VoteRecordRepository extends CrudRepository<VoteRecord, Long> {
+    Page<VoteRecord> findAll(Pageable pageable);
     List<VoteRecord> findByStatus(VoteRecordStatus pending);
 }
